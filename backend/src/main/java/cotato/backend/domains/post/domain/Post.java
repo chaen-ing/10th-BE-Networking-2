78c3a7c33d282a2330f86878c59aac0a9b59f137
chaen-ing <kimce0405@naver.com>
2024-11-16 15:37:26 +0900

    feat : Post 엔티티 생성

---
+++ backend/src/main/java/cotato/backend/domains/post/domain/Post.java
@@ -0,0 +1,40 @@
+package cotato.backend.domains.post.domain;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long postId;
+
+	@NonNull
+	private String title;
+
+	@NonNull
+	private String content;
+
+	@NonNull
+	private String name;
+
+	@ColumnDefault("0")
+	private Long views;
+}
